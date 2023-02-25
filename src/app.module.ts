c7e1cd3928725c0f4c3ccf79e91da869310c9e38
Javier <javier.gauna147@gmail.com>
2023-02-25 18:03:39 -0300

    feat(): agregado enviroments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.URLDB)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
