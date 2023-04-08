c5852724b437476f952dbb70c002388de503fe12
Javier <javier.gauna147@gmail.com>
2023-04-08 18:11:45 -0300

    feat() creada entidad para pedido

--- src/modulos/pedidos/pedidos.module.ts
+++ src/modulos/pedidos/pedidos.module.ts
@@ -4,10 +4,12 @@ import { EstadoPedidoService } from './estado-pedido/estado-pedido.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { EstadoPedido, EstadoPedidoSchema } from './estado-pedido/schemas/estado-pedido.schema';
+import { PedidoController } from './pedido/pedido.controller';
+import { PedidoService } from './pedido/pedido.service';
 
 @Module({
-  controllers: [EstadoPedidoController],
-  providers: [EstadoPedidoService],
+  controllers: [EstadoPedidoController, PedidoController],
+  providers: [EstadoPedidoService, PedidoService],
   imports: [
     MongooseModule.forFeature([
       {name: EstadoPedido.name, schema: EstadoPedidoSchema},
